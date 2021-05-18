333efb1e819b98edc0cd2cb4ac9f8e8c2bc2c89d
surajkumar <suraj.kumar@antiresolutions.com>
2021-05-18 18:46:43 +0530

    header, footer

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,34 @@
-import React from 'react'
-import './Header.scss'
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import Logo from "../../Assets/Images/logo.png";
+import Search from "../../Assets/Images/search.png";
+import "./Header.scss";
 
 function Header() {
-    return (
-        <div>
-            header
-        </div>
-    )
+  return (
+    <div className="custom_nav">
+      <Navbar expand="lg">
+        <Navbar.Brand href="#home">
+          <img src={Logo} alt="logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#home" className="active">
+              Home
+            </Nav.Link>
+            <Nav.Link href="#link">Categories</Nav.Link>
+            <Nav.Link href="#link">Activities</Nav.Link>
+            <Nav.Link href="#link">Trending</Nav.Link>
+            <Nav.Link href="#link">Create</Nav.Link>
+            <Nav.Link href="#link">About us</Nav.Link>
+            <Nav.Link href="#link">Contact</Nav.Link>
+          </Nav>
+          <img src={Search} alt="search" />
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,41 @@
-import React from 'react'
-import './Home.scss'
+import React from "react";
+import { Container, Row, Col, Tab, Tabs } from "react-bootstrap";
+import BannerImg from "../../Assets/Images/banner_img.png";
+import "./Home.scss";
 
 function Home() {
-    return (
-        <div>
-            home page
-        </div>
-    )
+  return (
+    <div>
+      <Container fluid className="top_banner">
+        <Tabs defaultActiveKey="new" id="uncontrolled-tab-example">
+          <Tab eventKey="new" title="New"></Tab>
+          <Tab eventKey="Art" title="Art"></Tab>
+          <Tab eventKey="Domain Names" title="Domain Names"></Tab>
+          <Tab eventKey="Virtual Worlds" title="Virtual Worlds"></Tab>
+          <Tab eventKey="Trading Cards" title="Trading Cards"></Tab>
+          <Tab eventKey="Collectibles" title="Collectibles"></Tab>
+          <Tab eventKey="Sports" title="Sports"></Tab>
+          <Tab eventKey="Utility" title="Utility"></Tab>
+        </Tabs>
+
+        <Row className="banner_row">
+          <Col>
+            <img src={BannerImg} />
+          </Col>
+          <Col>
+            <div className="banner_content">
+              <h1>The largest NFT marketplace</h1>
+              <p>Buy, sell, and discover rare digital items</p>
+              <div className="banner_btn">
+                <button className="dark_btn">Explore</button>
+                <button className="light_btn">Create</button>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
