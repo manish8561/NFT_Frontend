809144b1b0cdedc6120c86b72b8cbe13cec90bb4
Navdeep kaur <navdeep.kaur@antiersolutions.com>
2021-05-20 15:56:56 +0530

    new pages

--- src/Components/Popup/CreateCollectionpopup.js
+++ src/Components/Popup/CreateCollectionpopup.js
@@ -1,5 +1,5 @@
 import React, { Component, useState } from 'react'
-import { Modal, Button, Form } from "react-bootstrap";
+import { Modal, Button, Form, Col, Row } from "react-bootstrap";
 import './style.scss'
 function CreateCollectionpopup() {
     const [show, setShow] = useState(false);
@@ -18,20 +18,40 @@ function CreateCollectionpopup() {
                     <p>You can change these values later, along with configuring external URLs, payment <br></br>options, and trading fees.</p>
                 </Modal.Header>
                 <Modal.Body>
-                    <Form className="upload-doc">
+                    <Form>
 
-                        <Form.Group>
+                        <Form.Group className="upload-doc">
                             <div className="add-icon"><i className="fa fa-plus"></i></div>
                             <Form.File id="exampleFormControlFile1" />
+                            <p>Logo/ Image (350 x 350 recommended)</p>
+                        </Form.Group>
+                    </Form>
+
+                    <Form className="info-form">
+                        <Form.Group as={Row} controlId="formPlaintextEmail">
+                            <Form.Label column sm="2">
+                                Name*
+    </Form.Label>
+                            <Col sm="10">
+                                <Form.Control type="text" placeholder="Thomas George" />
+                            </Col>
+                        </Form.Group>
+
+                        <Form.Group as={Row} controlId="formPlaintextPassword">
+                            <Form.Label column sm="2">
+                                Description
+    </Form.Label>
+                            <Col sm="10">
+                                <Form.Control as="textarea" rows={4} placeholder="0 to 1000 characters used..." />
+
+                            </Col>
                         </Form.Group>
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-          </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
+
+                    <Button variant="primary" className=" read-btn" onClick={handleClose}>
+                        Create
           </Button>
                 </Modal.Footer>
             </Modal>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,5 @@
 import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
-import thumb from '../../Assets/Images/women.png'
-import CreateCollectionpopup from "../../Components/Popup/CreateCollectionpopup";
-import CollectionItems from "./CollectionItems";
 import "./Home.scss";
 
 function Home() {
@@ -39,73 +36,6 @@ function Home() {
         </Container>
       </Container>
 
-      <Container className="ContMain">
-        <Row className="banner_row">
-          <Col lg={9}>
-            <div className="collection_col">
-              <h2 className="main-heading">My Collections </h2>
-              <p>Create collections (your own storefronts), upload digital creations, configure your commission, and sell NFTs to your fans - all for free! You can also manage smart contracts that you created outside of NFT. Learn more</p>
-            </div>
-          </Col>
-
-        </Row>
-        <Row className="">
-          <Col lg={3}>
-            <div className="col-new">
-              <h3>Create new collection</h3>
-
-              <CreateCollectionpopup></CreateCollectionpopup>
-              <p>or add an existing contract</p>
-            </div>
-          </Col>
-          <Col lg={3}>
-            <div className="col-thumb">
-              <img src={thumb} />
-              <h3>Design Monitor</h3>
-            </div>
-          </Col>
-          <Col lg={3}>
-            <div className="col-thumb">
-              <img src={thumb} />
-              <h3>DV Artworks</h3>
-            </div>
-          </Col>
-          <Col lg={3}>
-            <div className="col-thumb">
-              <img src={thumb} />
-              <h3>Creative People</h3>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-
-      <Container fluid className="collection">
-        <Container className="custom-cont">
-          <Row>
-            <h4 className="main-heading">Collection items</h4>
-            <div className="coll-box">
-              <div className=" item-col">
-                <Row>
-                  <Col lg={3} className="p-0"> <CollectionItems thumb={thumb} title="William Leo " text="Art World" price="$ 155.99" ></CollectionItems></Col>
-                  <Col lg={3} className="p-0"> <CollectionItems thumb={thumb} title="William Leo " text="Art World" price="$ 155.99" ></CollectionItems></Col>
-                  <Col lg={3} className="p-0"> <CollectionItems thumb={thumb} title="William Leo " text="Art World" price="$ 155.99" ></CollectionItems></Col>
-                  <Col lg={3} className="p-0"> <CollectionItems thumb={thumb} title="William Leo " text="Art World" price="$ 155.99" ></CollectionItems></Col>
-                  <Col lg={3} className="p-0"> <CollectionItems thumb={thumb} title="William Leo " text="Art World" price="$ 155.99" ></CollectionItems></Col>
-                  <Col lg={3} className="p-0"> <CollectionItems thumb={thumb} title="William Leo " text="Art World" price="$ 155.99" ></CollectionItems></Col>
-                  <Col lg={3} className="p-0"> <CollectionItems thumb={thumb} title="William Leo " text="Art World" price="$ 155.99" ></CollectionItems></Col>
-                  <Col lg={3} className="p-0"> <CollectionItems thumb={thumb} title="William Leo " text="Art World" price="$ 155.99" ></CollectionItems></Col>
-                  <Col lg={3} className="p-0"> <CollectionItems thumb={thumb} title="William Leo " text="Art World" price="$ 155.99" ></CollectionItems></Col>
-                  <Col lg={3} className="p-0"> <CollectionItems thumb={thumb} title="William Leo " text="Art World" price="$ 155.99" ></CollectionItems></Col>
-                  <Col lg={3} className="p-0"> <CollectionItems thumb={thumb} title="William Leo " text="Art World" price="$ 155.99" ></CollectionItems></Col>
-                  <Col lg={3} className="p-0"> <CollectionItems thumb={thumb} title="William Leo " text="Art World" price="$ 155.99" ></CollectionItems></Col>
-
-                </Row>
-              </div>
-            </div>
-          </Row>
-
-        </Container>
-      </Container>
 
     </div>
   );
